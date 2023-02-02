5c2bfb96b6d5185db94dd4679d8b4f00be4db762
Lina Budkova <linabudkova35@gmail.com>
2023-02-02 22:32:56 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -14,7 +14,7 @@ function Section({ title, text, good, neutral, bad, total, positive, onGoodFeedb
         </div>
         <div>
                 <h2>{text}</h2>
-                {{total} === "0"
+                {total === 0
                     ? <Notification message='There is no feedback' />
                     : <Statistics good={good}
                     neutral={neutral}
